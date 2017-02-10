5814f7f7fc2d8d949e4c5fd9c5fc2e75558dedc8
suvaries <serdar.suvarierol@gmail.com>
2017-02-11 01:01:34 +0300

    first commit

---
+++ AaSampleService/AaSampleService.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace AaSampleService
+{
+    public partial class AaSampleService : ServiceBase
+    {
+        private Timer _timer;
+        public AaSampleService()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer();
+            //_timer.Enabled = true;
+            _timer.Interval = 10 * 1000;
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+
+            eventLog1.WriteEntry("Service started successfully", EventLogEntryType.Information);
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            eventLog1.WriteEntry("Service runs", EventLogEntryType.Information);
+        }
+        
+        protected override void OnStop()
+        {
+            _timer.Stop();
+            eventLog1.WriteEntry("Service stopped successfully", EventLogEntryType.Information);
+        }
+    }
+}
